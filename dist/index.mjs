18fbaa9ac7f6df57a0eca803a7f2bb2209439bc8
Nuszpl Botond <nuszplbotond@gmail.com>
2023-12-15 19:16:40 +0100

    Log

--- dist/index.mjs
+++ dist/index.mjs
@@ -52855,7 +52855,12 @@ async function sendSlackReleaseNotes(version) {
     addDivider: core.getInput("addDivider") ?? true,
     channel: core.getInput("channel") ?? "",
     repostChannels: core.getInput("repostChannels") ?? "",
+    SLACK_BOT_TOKEN: core.getInput("SLACK_BOT_TOKEN") ?? "",
   };
+
+  console.log("input", input);
+
+  return 
 
   if (!input.channel) {
     throw new Error("Channel is not set");
@@ -53056,9 +53061,6 @@ async function sendSlackReleaseNotes(version) {
     ],
   };
 
-  const SLACK_BOT_TOKEN =
-    "xoxb-6269943513543-6307394151552-7Bz7C1zzc4upuXQO2PIV1s5F";
-
   const slackAPIResponse = await fetch(
     "https://slack.com/api/chat.postMessage",
     {
@@ -53066,7 +53068,7 @@ async function sendSlackReleaseNotes(version) {
       body: JSON.stringify(slackPayload),
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${SLACK_BOT_TOKEN}`,
+        Authorization: `Bearer ${input.SLACK_BOT_TOKEN}`,
       },
     },
   );
@@ -53091,7 +53093,7 @@ async function createRelease(version) {
     throw new Error("GITHUB_REPOSITORY is not set");
   }
 
-  if (process.env.SLACK_BOT_TOKEN)
+  if (/* core.getInput("SLACK_BOT_TOKEN") */ true)
     await sendSlackReleaseNotes(version.toString());
 
   await octokit.request("POST /repos/{owner}/{repo}/releases", {
